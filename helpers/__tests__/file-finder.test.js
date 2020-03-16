8f9d9df885a1e76a89e5ba41e5b60684794b7ce9
Chua Colin <colin@hive-up.com>
2020-03-17 00:44:02 +0800

    [FEATURE-6] Added unit test

--- helpers/__tests__/file-finder.test.js
+++ helpers/__tests__/file-finder.test.js
@@ -1,6 +1,24 @@
+const mock = require('mock-fs')
+const fileFinder = require('../file-finder');
+
+beforeEach(() => {
+    mock({
+        'path/to/fake/dir': {
+          'some-file.js': 'file content here',
+          'not-empty-dir': {
+              'some-file1.js': 'file content here'
+          }
+        }
+    });
+})
+
+afterEach(mock.restore);
+
 describe('fileFinder', () => {
-    it('should do testing for fileFinder', () => {
-        const finder = 'fileFinder'
-        expect(finder).toBe('fileFinder')
-    })
+    it('will receive 2 files if total files in the folder is two', () => {
+        const fileSummary = fileFinder('path/to');
+
+        expect(fileSummary.length).toBe(2);
+        expect(fileSummary).toMatchObject(["path/to/fake/dir/not-empty-dir/some-file1.js", "path/to/fake/dir/some-file.js"])
+      });
 })
--- helpers/__tests__/word-finder.test.js
+++ helpers/__tests__/word-finder.test.js
@@ -1,6 +1,24 @@
+const mock = require('mock-fs')
+const wordFinder = require('../word-finder');
+
+beforeEach(() => {
+    mock({
+        'path/to/fake/dir': {
+          'some-file.js': 'file content here Hello',
+          'not-empty-dir': {
+              'some-file1.js': 'file content here'
+          }
+        }
+    });
+})
+
+afterEach(mock.restore);
+
 describe('wordFinder', () => {
-    it('should do testing for wordFinder', () => {
-        const finder = 'wordFinder'
-        expect(finder).toBe('wordFinder')
-    })
+    it('should return only files with certain words on it', () => {
+        const fileList = ["path/to/fake/dir/not-empty-dir/some-file1.js", "path/to/fake/dir/some-file.js"]
+        const results = wordFinder(fileList, 'Hello');
+
+        expect(results).toMatchObject(["path/to/fake/dir/some-file.js"])
+      });
 })
