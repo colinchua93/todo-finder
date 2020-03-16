f6a7b979562984233c9204e61874c184993a3f69
Chua Colin <colin@hive-up.com>
2020-03-16 22:29:47 +0800

    [FEATURE-6] Add docker container

---
+++ scripts/devstart.sh
@@ -0,0 +1,2 @@
+docker-compose build --no-cache
+docker-compose -f ../docker-compose.yml up -d --remove-orphans
---
+++ scripts/remove.sh
@@ -0,0 +1,3 @@
+docker stop todo_finder
+docker container rm todo_finder
+docker image rm todos-finder_todo_finder
